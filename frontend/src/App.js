3a7282318368e08c57399a1d2d0b6f1254424ccf
Easton <n11927399@qut.edu.au>
2025-08-11 11:24:03 +1000

    auth(fix)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import Login from './pages/Login';
 import Register from './pages/Register';
 import Profile from './pages/Profile';
 import Tasks from './pages/Tasks';
-import {useAuth} from './contect/AuthCOntext';
+import {useAuth} from './context/AuthContext';
 
 function App() {
   const { user } = useAuth();
