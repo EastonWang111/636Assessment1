e0202839bd40aa26334bfb327a4fa8a86a4f0c96
Easton <n11927399@qut.edu.au>
2025-08-16 10:25:13 +1000

    fix

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   //baseURL: 'http://localhost:5001', // local
-  baseURL: 'http://52.64.199.195/api',// live
+  baseURL: 'http://52.64.199.195',// live
   headers: { 'Content-Type': 'application/json' },
 });
 
